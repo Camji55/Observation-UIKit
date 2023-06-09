204b20f0e0f8315ee4bbe564fea3acc16403f66c
Cameron Ingham <camji555@gmail.com>
2023-06-09 15:43:13 -0700

    Initial Commit

--- Observation UIKit/ViewController.swift
+++ Observation UIKit/ViewController.swift
@@ -5,15 +5,51 @@
 //  Created by Cameron Ingham on 6/9/23.
 //
 
+import Observation
 import UIKit
+
+@Observable
+final class Counter {
+    
+    var count = 0
+    
+    func increment() {
+        count += 1
+    }
+    
+    func decrement() {
+        count -= 1
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    let counter = Counter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        render()
     }
-
-
+    
+    private func render() {
+        withObservationTracking {
+            countLabel.text = "\(counter.count)"
+        } onChange: { [weak self] in
+            Task { @MainActor [weak self] in
+                self?.render()
+            }
+        }
+    }
+    
+    @IBAction func incrementButtonTapped(_ sender: Any) {
+        counter.increment()
+    }
+    
+    @IBAction func decrementButtonTapped(_ sender: Any) {
+        counter.decrement()
+    }
 }
 
